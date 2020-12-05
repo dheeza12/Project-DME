cdd74ee0ca608bc2734001ddbcbd7bb932e50d10
dheeza12 <teena@kkumail.com>
2020-12-05 22:31:17 +0700

    yyyyyyy

--- Message Simulator v0.1/mainqt5.py
+++ Message Simulator v0.1/mainqt5.py
@@ -1,5 +1,5 @@
 import sys
-from TC import TextAndChoice
+from main import TextAndChoice
 from PyQt5.QtWidgets import (
     QApplication, QLineEdit, QComboBox,
     QWidget, QFormLayout, QPushButton
@@ -13,11 +13,11 @@ class CreateMenu(QWidget):
         self.init_ui(tc)
 
     def init_ui(self, tc):
-        f_layout = QFormLayout()
+        self.f_layout = QFormLayout()
 
         self.c_box = QComboBox()
         self.c_box.currentTextChanged.connect(self.cbox_change)
-        f_layout.addRow(self.c_box)
+        self.f_layout.addRow(self.c_box)
 
         self.choice_line = QLineEdit()
         self.text_line = QLineEdit()
@@ -30,34 +30,39 @@ class CreateMenu(QWidget):
         self.to_start_but = QPushButton('Go to Start')
         self.next_path_but = QPushButton('Go into Path')
 
-        f_layout.addRow(self.cur_choice_line)
-        f_layout.addRow(self.choice_line, self.text_line)
-        f_layout.addRow(self.next_path_but)
-        f_layout.addRow(self.back_but, self.to_start_but)
-        f_layout.addRow(self.apply_but, self.add_but)
+        self.f_layout.addRow(self.cur_choice_line)
+        self.f_layout.addRow(self.choice_line, self.text_line)
+        self.f_layout.addRow(self.next_path_but)
+        self.f_layout.addRow(self.back_but, self.to_start_but)
+        self.f_layout.addRow(self.apply_but, self.add_but)
 
-        self.setLayout(f_layout)
+        self.setLayout(self.f_layout)
         self.show()
         self.access_node(tc)
 
     def access_node(self, node):
+        self.btn_disconnect()
         self.c_box.clear()
         self.choice_line.setText('')
         self.cur_choice_line.setText(node.choice_text)
-        print(node.choice_text)
         self.text_line.setText(node.text)
         if not node.is_end():
             for i in node.path:
                 self.c_box.addItem(i.choice_text)
         self.c_box.currentIndexChanged.connect(self.cbox_change)
-
         self.apply_but.clicked.connect(lambda x, y=node: self.apply_change(y))
         self.add_but.clicked.connect(lambda x, y=node: self.add_choice(y))
-        self.next_path_but.clicked.connect(
-            lambda x, y=node: self.next_path_line(y)
-        )
+        self.next_path_but.clicked.connect(lambda x, y=node: self.next_path_line(y))
         self.back_but.clicked.connect(lambda x, y=node: self.back_node(y))
         self.to_start_but.clicked.connect(lambda x, y=node: self.to_start(y))
+
+    def btn_disconnect(self):
+        self.apply_but.disconnect()
+        self.add_but.disconnect()
+        self.back_but.disconnect()
+        self.to_start_but.disconnect()
+        self.next_path_but.disconnect()
+        self.c_box.disconnect()
 
     def cbox_change(self):
         self.choice_line.setText(str(self.c_box.currentText()))
@@ -69,15 +74,15 @@ class CreateMenu(QWidget):
         self.c_box.setItemText(i, node.path[i].choice_text)
 
     def add_choice(self, node):
-        add = TextAndChoice(text=None, choice_text=None)
+        add = TextAndChoice('Bruh')
         node.add_path(add)
         self.c_box.addItem(add.choice_text)
 
     def next_path_line(self, node):
-        if node.is_end():
-            print("No path available")
+        if not node.is_end():
+            self.access_node(node.path[0])
         else:
-            self.access_node(node.path[self.c_box.currentIndex()])
+            print("No path available")
 
     def back_node(self, node):
         self.access_node(node.back)
