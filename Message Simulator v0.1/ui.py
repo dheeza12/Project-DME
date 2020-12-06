2a417583130da7b8afc81306b722b7851b895fd2
dheeza12 <teena@kkumail.com>
2020-12-06 12:45:21 +0700

    change directory for img and a little change to design

--- Message Simulator v0.1/ui.py
+++ Message Simulator v0.1/ui.py
@@ -6,32 +6,46 @@ from main import TextAndChoice
 
 
 class PlayUi(QScrollArea):
-    def __init__(self, root):
+    def __init__(self, root, name='Play Mode'):
         super().__init__()
         self.root = root
         self.mainWidget = QWidget()
-
+        self.show()
         self.box = QVBoxLayout()
         self.box.setAlignment(Qt.AlignTop)
         self.mainWidget.setLayout(self.box)
-        self.mainWidget.setFont(QFont('Arial', 12))
+        self.mainWidget.setObjectName("Background")
 
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.setFixedSize(600, 800)
         self.setWidgetResizable(True)
         self.setCenter()
-        self.setWindowTitle("Play demo")
+        self.setWindowTitle(name.split('.')[0])
         self.setWidget(self.mainWidget)
-        self.setStyleSheet("""  QLabel#Choice { background-color: Turquoise; padding: 10px;
-                                    font-size: 16px; font-family: Arial;} 
-                                
-                                QLabel#Label  { background-color: Mintcream; color: black;
-                                    border-style: ridge; border-width: 5px; border-color: snow; border-radius: 10px;
-                                    padding: 6px; font-size: 16px; font-family: Arial}
-                                
-                                QPushButton#Choice { font-size: 16px; font-family: Arial; font: bold;
-                                    }""")
-        self.show()
+        self.setFont(QFont('Helvetica', 18))
+        self.setStyleSheet("""  
+        QWidget#Background {background-color: Lightcyan}
+        
+        QLabel#Label  { background-color: snow; color: black; font-size: 16px;
+            border-style: ridge; border-width: 5px; border-color: Mintcream; border-radius: 10px;
+            padding: 6px; padding-right: 12px; padding-left: 12px;
+            }
+        
+        QLabel#Choice { background-color: Deepskyblue; 
+            font-size: 18px; color: White; border-radius: 5px;
+            border-style: solid; border-width: 4px; border-color: deepskyblue;
+            min-width: 377; min-height: 64; padding-left: 20px; padding-right: 20px; padding-top: 10px;
+            padding-bottom: 5px;
+            } 
+                                                    
+        QPushButton#Choice { background-color: Azure; font-size: 16px; font-family: Arial; font: bold; 
+            border-style: outset; border-width: 6px; border-color: Aqua; border-radius: 15px;
+            padding: 10px;
+            }
+        QPushButton#Choice:hover { background-color: Mintcream; border-color: Chartreuse;}
+            }
+                            """)
+
         self.play(self.root)
 
     def play(self, root):
@@ -39,41 +53,51 @@ class PlayUi(QScrollArea):
         vbox = QVBoxLayout()
         if root.actor:              # ADD NAME IF EXISTED
             actor_label = QLabel(root.actor+':')
+            actor_label.setObjectName('Actor')
             vbox.addWidget(actor_label)
         line_label = QLabel(str(root))
         line_label.setObjectName('Label')
         line_label.setWordWrap(True)
-
         vbox.addWidget(line_label)
 
-        pix_label = QLabel()
         if root.img:
-            pixMap = QPixmap(root.img)
+            pix_map = QPixmap(root.img)
         else:
-            pixMap = QPixmap('default_avatar.png')
-        pixMap = pixMap.scaled(64, 64, Qt.IgnoreAspectRatio, Qt.FastTransformation)
-        pix_label.setPixmap(pixMap)
+            pix_map = QPixmap('Content/default_avatar.png')
+        pix_map = pix_map.scaled(64, 64, Qt.IgnoreAspectRatio, Qt.FastTransformation)
+        pix_label = QLabel()
+        pix_label.setPixmap(pix_map)
         pix_label.setObjectName('Pixmap')
+
+        if root.main_img:
+            main_pix_map = QPixmap(root.main_img)
+        else:
+            main_pix_map = QPixmap('Content/default_avatar.png')
+        main_pix_map = main_pix_map.scaled(64, 64, Qt.IgnoreAspectRatio, Qt.FastTransformation)
+        main_pix_label = QLabel()
+        main_pix_label.setPixmap(main_pix_map)
+        main_pix_label.setObjectName('Pixmap')
 
         if root.main:
             hbox.setAlignment(Qt.AlignRight)
             hbox.addLayout(vbox)
-            hbox.addWidget(pix_label)
+            hbox.addWidget(main_pix_label)
         else:
             hbox.setAlignment(Qt.AlignLeft)
             hbox.addWidget(pix_label)
             hbox.addLayout(vbox)
-
         self.box.addLayout(hbox)
 
         if not root.is_end():
             if len(root) > 1:
                 hbox = QHBoxLayout()
                 vbox = QVBoxLayout()
+                vbox.setAlignment(Qt.AlignCenter)
 
                 for i, choice in enumerate(root.path):
                     widget = QWidget()
                     vbox_sub = QVBoxLayout()
+                    vbox_sub.setAlignment(Qt.AlignCenter)
                     widget.setLayout(vbox_sub)
 
                     btnLabel = QLabel(choice.choice_text)
@@ -89,8 +113,6 @@ class PlayUi(QScrollArea):
                     vbox.addWidget(widget)
                     vbox_sub.addWidget(butt)
                 hbox.addLayout(vbox)
-
-
                 self.box.addLayout(hbox)
 
             elif len(root) != 0:
@@ -126,5 +148,5 @@ if __name__ == '__main__':
     file = json.load(file)
     decoded = TextAndChoice.decode(file)
     app = QApplication(sys.argv)
-    play = PlayUi(decoded)
+    play = PlayUi(decoded, 'IamDME.json')
     sys.exit(app.exec_())
